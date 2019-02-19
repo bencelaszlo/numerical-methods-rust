fe0e69a6713870d7508d8b3efbdf4500983ca1ae
bencelaszlo <bencelaszlo@protonmail.com>
2019-02-19 23:45:44 +0100

    Fix array interpolation when using unsorted paramter arrays

--- src/interpolation.rs
+++ src/interpolation.rs
@@ -1,13 +1,12 @@
 fn linear_interpolation(x1: f32, y1: f32, x0: f32, y0: f32, x: f32) -> f32 {
     let delta: f32;
     delta = x1 - x0;
-
     let y;
 
     if delta == 0.0 {
         y = y0;
     } else {
-        y = y0 + (x - x0 / delta) * y1;
+        y = y0 + ( (x - x0) / delta) * y1;
     }
 
     return y;
@@ -15,20 +14,29 @@ fn linear_interpolation(x1: f32, y1: f32, x0: f32, y0: f32, x: f32) -> f32 {
 
 pub fn linear_interpolation_array(X: &[f32], Y: &[f32], x: &[f32], y: &mut [f32]) {
     for i in 0..x.len() {
-        let lower_bound_index: usize;
-        let higher_bound_index: usize;
-        
-        let mut j: usize = 0;
-        while X[j] < x[i] {
-            j += 1;
-        }
-        lower_bound_index = j - 1;
+        let mut lower_bound_index: usize = 0;
+        let mut higher_bound_index: usize = 0;
 
-        j = X.len() - 1;
-        while X[j] > x[i] {
-            j -= 1;
+         for j in 0..X.len() {
+            if X[j] < X[lower_bound_index] {
+                lower_bound_index = j;
+            }
+
+            if X[j] > X[higher_bound_index] {
+                higher_bound_index = j;
+            }
         }
-        higher_bound_index = j+1;
+
+        
+        for j in 0..X.len() {
+            if X[j] <= x[i] && X[j] > X[lower_bound_index] {
+                lower_bound_index = j;
+            }
+
+            if X[j] >= x[i] && X[j] < X[higher_bound_index] {
+                higher_bound_index = j;
+            }
+        }
 
         y[i] = linear_interpolation(X[higher_bound_index], Y[higher_bound_index], X[lower_bound_index], Y[lower_bound_index], x[i]);
     }
